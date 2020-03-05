abd12bb0707d5689f19a3d348647e02be928346a
Hans Wustrack <hans2792@gmail.com>
2020-03-05 15:13:27 +1300

    start analysis

--- analysis.py
+++ analysis.py
@@ -7,24 +7,79 @@ from posts_db import sqlite_file, topics_table_name
 def main():
     analyze()
 
+
 def analyze():
     conn = sqlite3.connect(sqlite_file)
+
+    tags_by_month(conn, get_months())
+
+    conn.close()
+
+def tags_by_month(conn, months):
     c = conn.cursor()
 
-    query = '''
-        SELECT t.name, count(m.topic_id) count, sum(p.votesCount) votes, 
-            sum(p.votesCount) / count(m.topic_id) norm
+    for i in range(len(months) - 1):
+        print(months[i])
+
+        query = '''
+        SELECT t.name, count(m.topic_id) count
         FROM post_topics m 
         JOIN topics t ON t.rowid = m.topic_id
         JOIN posts p on p.rowid = m.post_id
+        WHERE p.createdAt > "{after}" AND p.createdAt < "{before}"
+        GROUP BY m.topic_id
+        ORDER BY count DESC
+        LIMIT 3;
+        '''.format(after=months[i], before=months[i + 1])
+        c.execute(query)
+        results = c.fetchall()
+        pprint(results)
+
+        query = '''
+        SELECT t.name, count(m.topic_id) count, sum(p.votes_count) votes, 
+            sum(p.votes_count) / count(m.topic_id) mean
+        FROM post_topics m 
+        JOIN topics t ON t.rowid = m.topic_id
+        JOIN posts p on p.rowid = m.post_id
+        WHERE p.createdAt > "{after}" AND p.createdAt < "{before}"
         GROUP BY m.topic_id
         ORDER BY norm DESC
-        '''
-    c.execute(query)
-    results = c.fetchall()
-    pprint(results)
+        LIMIT 3;
+        '''.format(after=months[i], before=months[i + 1])
+        c.execute(query)
+        results = c.fetchall()
+        pprint(results)
 
-    conn.close()
+        query = '''
+        SELECT count(*)
+        FROM posts
+        WHERE createdAt > "{after}" AND createdAt < "{before};"
+        '''.format(after=months[i], before=months[i + 1])
+        c.execute(query)
+        results = c.fetchall()
+        pprint(results)
+
+        print()
+
+
+def get_months():
+    months = []
+
+    year = "2019"
+    for m in range(12):
+        m = m + 1
+        if m < 10:
+            months.append(year + "-0" + str(m) + "-01T00:00:00Z")
+        else:
+            months.append(year + "-" + str(m) + "-01T00:00:00Z")
+
+    year = "2020"
+    for m in range(3):
+        m = m + 1
+        months.append(year + "-0" + str(m) + "-01T00:00:00Z")
+
+    return months
+
 
 if __name__ == '__main__':
     main()
--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-SELECT * FROM posts LIMIT 20;
+SELECT * FROM posts LIMIT 5;
 SELECT * FROM topics LIMIT 5;
 SELECT * FROM post_topics LIMIT 5;
 
@@ -18,7 +18,7 @@ JOIN posts p on p.rowid = m.post_id
 LIMIT 5;
 
 SELECT t.name, count(m.topic_id) count, sum(p.votes_count) votes, 
-    sum(p.votes_count) / count(m.topic_id) norm
+    sum(p.votes_count) / count(m.topic_id) mean
 FROM post_topics m 
 JOIN topics t ON t.rowid = m.topic_id
 JOIN posts p on p.rowid = m.post_id
@@ -32,4 +32,16 @@ JOIN topics t ON t.rowid = m.topic_id
 JOIN posts p on p.rowid = m.post_id
 GROUP BY m.topic_id
 ORDER BY count ASC, votes DESC 
+LIMIT 20;
+
+SELECT * FROM posts WHERE createdAt < "2019-02-01T00:00:00Z" LIMIT 5;
+SELECT count(*) FROM post_topics WHERE topic_id = 14;
+
+SELECT t.name, m.topic_id, count(m.topic_id) count
+FROM post_topics m 
+JOIN topics t ON t.rowid = m.topic_id
+JOIN posts p on p.rowid = m.post_id
+WHERE p.createdAt < "2019-02-01T00:00:00Z" and p.createdAt > "2019-01-01T00:00:00Z"
+GROUP BY m.topic_id
+ORDER BY count DESC
 LIMIT 20;
